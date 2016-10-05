e12083dfa47d90841e83c613d09286479f346c80
shuaiz28 <shuaiz@bu.edu>
2016-10-05 10:55:18 -0400

    deleted css property from main.js

--- js/main.js
+++ js/main.js
@@ -2,22 +2,18 @@ var app = angular.module('routingApp', ['ngRoute']);
 app.config(function($routeProvider) {
     $routeProvider
         .when('/', {
-            templateUrl: 'pages/main.html',
-            css: 'styleSheets/main.css'
+            templateUrl: 'pages/main.html'
         })
         .when('/page1', {
             templateUrl: 'pages/page1.html',
-            controller: 'controller1',
-            css: 'styleSheets/page1.css'
+            controller: 'controller1'
         })
         .when('/page2', {
             templateUrl: 'pages/page2.html',
-            controller: 'controller2',
-            css: 'styleSheets/page2.css'
+            controller: 'controller2'
         })
         .when('/page3', {
             templateUrl: 'pages/page3.html',
-            controller: 'controller3',
-            css: 'styleSheets/page3.css'
+            controller: 'controller3'
         })
 })
